39921895dae0bc3dbaa8b6048998ffa9d98bed16
davidmuir1999 <105323893+davidmuir1999@users.noreply.github.com>
2022-10-04 21:47:26 +0100

    Add files via upload

--- SW3 Website Project/app.js
+++ SW3 Website Project/app.js
@@ -1,10 +1,21 @@
-let isModalOpen = false;
+let isModalOpen1 = false;
 
-function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
+function toggleModal1() {
+    if (isModalOpen1) {
+        isModalOpen1 = false;
         return document.body.classList.remove("modal--open");
     }
-    isModalOpen = true;
+    isModalOpen1 = true;
+    document.body.classList += " modal--open";
+}
+
+let isModalOpen2 = false;
+
+function toggleModal2() {
+    if (isModalOpen2) {
+        isModalOpen2 = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen2 = true;
     document.body.classList += " modal--open";
 }
