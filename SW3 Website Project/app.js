1f852e4cd43928f3d82b6830e5b0987fcdefcee0
davidmuir1999 <105323893+davidmuir1999@users.noreply.github.com>
2022-10-13 21:58:55 +0100

    Add files via upload

--- SW3 Website Project/app.js
+++ SW3 Website Project/app.js
@@ -1,3 +1,4 @@
+let isModalOpen = false;
 let isModalOpen1 = false;
 let isModalOpen2 = false;
 
@@ -16,5 +17,38 @@ function toggleModal2() {
         return document.body.classList.remove("modal--open");
     }
     isModalOpen2 = true;
+    document.body.classList += " modal--open";
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible"
+
+    emailjs
+        .sendForm(
+            "service_a32uvxl",
+            "template_i552roh",
+            event.target,
+            "dSlgm8_3vYYWH9niU"
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me at davidmuir1999@gmail.com"
+            );
+        })
+}
+
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
     document.body.classList += " modal--open";
 }
