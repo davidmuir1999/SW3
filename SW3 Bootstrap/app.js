a277e6fc53dccd722643d4bde9bd2426c68e914a
davidmuir1999 <105323893+davidmuir1999@users.noreply.github.com>
2022-11-03 22:48:51 +0000

    Add files via upload

--- SW3 Bootstrap/app.js
+++ SW3 Bootstrap/app.js
@@ -5,4 +5,44 @@ function openMenu() {
   function closeMenu() {
     document.body.classList.remove('menu--open')
   }
+
+  let isModalOpen = false;
+
+  function contact(event) {
+      event.preventDefault();
+      const loading = document.querySelector('.modal__overlay--loading');
+      const success = document.querySelector('.modal__overlay--success');
+      loading.classList += " modal__overlay--visible"
   
+      // emailjs
+      //     .sendForm(
+      //         "service_a32uvxl",
+      //         "template_i552roh",
+      //         event.target,
+      //         "dSlgm8_3vYYWH9niU"
+      //     ).then(() => {
+      //         loading.classList.remove("modal__overlay--visible");
+      //         success.classList += " modal__overlay--visible";
+      //     }).catch(() => {
+      //         loading.classList.remove("modal__overlay--visible");
+      //         alert(
+      //             "The email service is temporarily unavailable. Please contact me at davidmuir1999@gmail.com"
+      //         );
+      //     })
+  
+      setTimeout(() => {
+          loading.classList.remove("modal__overlay--visible")
+          success.classList += " modal__overlay--visible"
+          console.log('it worked')
+      },1500);
+  }
+  
+  
+  function toggleModal() {
+      if (isModalOpen) {
+          isModalOpen = false;
+          return document.body.classList.remove("modal--open");
+      }
+      isModalOpen = true;
+      document.body.classList += " modal--open";
+  }
